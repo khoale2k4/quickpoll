8eaaac920962bbf6459b8f829665f3a504f790a2
KhoaLe <levodangkhoatg2@gmail.com>
2025-03-13 18:39:13 +0700

    some api

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,24 @@
 import { Settings } from "lucide-react";
 
-const Card = ({ title, value, unit, description, mode, bgColor, icon, onSettingsClick }) => (
-    <div className={`relative p-4 rounded-2xl shadow-md flex flex-col md:flex-row justify-between items-center ${bgColor} text-white transition-transform hover:scale-105 `}>
-        <Settings 
-            className="absolute top-2 right-2 text-white cursor-pointer" 
-            onClick={onSettingsClick} 
-        />
-        
-        <div className="flex-1 text-center md:text-left ">
-            <h3 className="text-lg font-semibold flex items-center justify-center md:justify-start ">{icon} {title}</h3>
-            <p className="text-4xl font-bold">
-                {value} <span className="text-xl">{unit}</span>
-            </p>
-            <p className="text-sm opacity-80">{description}</p>
+const Card = ({ title, value, unit, description, mode, bgColor, icon, onSettingsClick }) => {
+    const available = !(value === -1);
+    return (
+        <div className={`relative p-4 rounded-2xl shadow-md flex flex-col md:flex-row justify-between items-center ${available?bgColor:"bg-gray-400"} text-white transition-transform hover:scale-105 `}>
+            <Settings
+                className="absolute top-2 right-2 text-white cursor-pointer"
+                onClick={available?onSettingsClick:null}
+            />
+
+            <div className="flex-1 text-center md:text-left ">
+                <h3 className="text-lg font-semibold flex items-center justify-center md:justify-start ">{icon} {title}</h3>
+                <p className="text-4xl font-bold">
+                    {available?value:"No data"} <span className="text-xl">{available ?unit:""}</span>
+                </p>
+                <p className="text-sm opacity-80">{available?description:"No data"}</p>
+            </div>
+            <div className="text-lg font-semibold mt-2 pr-10 md:mt-0 ">{mode}</div>
         </div>
-        <div className="text-lg font-semibold mt-2 pr-10 md:mt-0 ">{mode}</div>
-    </div>
-);
+    )
+};
 
 export default Card;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import { Bell, BarChart, User, History  } from "lucide-react";
+import { Bell, BarChart, User, History } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Card from "../components/Card.js";
@@ -7,16 +7,96 @@ import LightSettingPopup from "../components/LightSettingPopup.js";
 import TemperaturePopup from "../components/TemperatureSettingPopup.js";
 
 import { LuLeaf } from "react-icons/lu";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function YoloFarmDashboard() {
   const [isSettingIrrigation, setSettingIrrigation] = useState(false);
   const [isSettingTemperature, setSettingTemperature] = useState(false);
   const [isSettingLighting, setSettingLighting] = useState(false);
-  
+  const [infoMap, setInfoMap] = useState({
+    irrigation: {
+      value: -1,
+      mode: null,
+      description: ""
+    },
+    temperture: {
+      value: -1,
+      mode: null,
+      description: ""
+    },
+    lightning: {
+      value: -1,
+      mode: null,
+      description: ""
+    }
+  })
+
   const navigate = useNavigate();
   const handleIconClick = (value) => () => {
     navigate(`/${value}`);
   };
+
+  useEffect(() => {
+    const fetchMoistureData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_HOST}/api/records/moisture/1`);
+        const data = response.data;
+        const latestData = data[data.length - 1];
+        setInfoMap(prevInfoMap => ({
+          ...prevInfoMap,
+          irrigation: {
+            ...prevInfoMap.irrigation,
+            value: latestData.recordValue
+          }
+        }));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const fetchTemperatureData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_HOST}/api/records/temperature/1`);
+        const data = response.data;
+        const latestData = data[data.length - 1];
+        setInfoMap(prevInfoMap => ({
+          ...prevInfoMap,
+          temperture: {
+            ...prevInfoMap.temperture,
+            value: latestData.recordValue
+          }
+        }));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const fetchLightningData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_HOST}/api/records/light/1`);
+        const data = response.data;
+        const latestData = data[data.length - 1];
+        setInfoMap(prevInfoMap => ({
+          ...prevInfoMap,
+          lightning: {
+            ...prevInfoMap.lightning,
+            value: latestData.recordValue
+          }
+        }));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    const intervalId = setInterval(() => {
+      fetchMoistureData();
+      fetchTemperatureData();
+      fetchLightningData();
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="p-6 max-w-lg md:max-w-2xl mx-auto space-y-6">
@@ -24,57 +104,57 @@ export default function YoloFarmDashboard() {
         <div className="w-16 h-16 bg-green-200 flex items-center justify-center rounded-xl">
           <LuLeaf className="w-12 h-12 text-green-500 " />
         </div>
-        
+
         <div className="text-center md:text-left ml-4">
           <h1 className="text-2xl font-bold">YOLO FARM</h1>
           <p className="text-gray-600">Hello, Group 72</p>
         </div>
 
         <div className="flex space-x-4 text-green-500 ml-auto">
-          <History 
+          <History
             className="cursor-pointer hover:text-green-700"
             onClick={handleIconClick("action")}
-            />
-          <Bell 
-            className="cursor-pointer hover:text-green-700" 
+          />
+          <Bell
+            className="cursor-pointer hover:text-green-700"
             onClick={handleIconClick("reminder")}
           />
-          <BarChart 
-            className="cursor-pointer hover:text-green-700" 
-            onClick={handleIconClick("statistics")}/>
-          <User 
-            className="cursor-pointer hover:text-green-700" 
-            onClick={handleIconClick("profile")} 
-          />      
+          <BarChart
+            className="cursor-pointer hover:text-green-700"
+            onClick={handleIconClick("statistics")} />
+          <User
+            className="cursor-pointer hover:text-green-700"
+            onClick={handleIconClick("profile")}
+          />
         </div>
       </div>
 
       <Card
         title="Irrigation"
-        value="15"
+        value={infoMap.irrigation.value}
         unit="%"
-        description="Keep around 10%"
-        mode="AUTOMATIC"
-        bgColor="bg-blue-400"
+        description={infoMap.irrigation.description}
+        mode={infoMap.irrigation.mode}
+        bgColor={"bg-blue-400"}
         icon="💧"
         onSettingsClick={() => setSettingIrrigation(true)}
       />
       <Card
         title="Temperature"
-        value="30"
+        value={infoMap.temperture.value}
         unit="°C"
-        description="Sunscreens between 08 AM - 11 AM"
-        mode="SCHEDULED"
+        description={infoMap.temperture.description}
+        mode={infoMap.temperture.mode}
         bgColor="bg-red-400"
         icon="🌡"
         onSettingsClick={() => setSettingTemperature(true)}
       />
       <Card
         title="Lighting"
-        value="600"
+        value={infoMap.lightning.value}
         unit="W/m²"
-        description="Click to turn off lamps"
-        mode="MANUAL"
+        description={infoMap.lightning.description}
+        mode={infoMap.lightning.mode}
         bgColor="bg-green-700"
         icon="☀️"
         onSettingsClick={() => setSettingLighting(true)}
@@ -83,7 +163,7 @@ export default function YoloFarmDashboard() {
       {isSettingTemperature && <TemperaturePopup isOpen={isSettingTemperature} onClose={() => setSettingTemperature(false)} />}
       {isSettingLighting && <LightSettingPopup isOpen={isSettingLighting} onClose={() => setSettingLighting(false)} />}
     </div>
-    
+
   );
 
 }
