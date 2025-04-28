9a48d3507ab5f24a6293560b92d653ec41b33af4
KhoaLe <levodangkhoatg2@gmail.com>
2025-04-28 11:09:48 +0700

    add socket

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const http = require('http');
+const socketIo = require('socket.io');
 
 // Load environment variables
 dotenv.config();
 
 // Create Express app
 const app = express();
+// socket
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 // Middleware
 app.use(express.json());
@@ -23,6 +33,13 @@ const pollsRoutes = require('./routes/polls');
 app.use('/api', apiRoutes);
 app.use('/polls', pollsRoutes);
 
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -31,6 +48,6 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
